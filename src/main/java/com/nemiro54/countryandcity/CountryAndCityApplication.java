50cfe2df8ceffe7fe3facc4a6058c25b62773a0f
Uladzislau Niamira <nemiro54@gmail.com>
2023-08-18 19:23:26 +0300

    initial commit

---
+++ src/main/java/com/nemiro54/countryandcity/CountryAndCityApplication.java
@@ -0,0 +1,13 @@
+package com.nemiro54.countryandcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryAndCityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CountryAndCityApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/nemiro54/countryandcity/CountryAndCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemiro54.countryandcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryAndCityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
