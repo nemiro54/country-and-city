92f8e42947d113fd08ad76f738ef2ccc1e3c08a3
Uladzislau Niamira <nemiro54@gmail.com>
2023-08-18 19:26:05 +0300

    change application.properties to application.yml, add context loaded test

--- src/test/java/com/nemiro54/countryandcity/CountryAndCityApplicationTests.java
+++ src/test/java/com/nemiro54/countryandcity/CountryAndCityApplicationTests.java
@@ -1,5 +1,7 @@
 package com.nemiro54.countryandcity;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,8 @@ class CountryAndCityApplicationTests {
 
   @Test
   void contextLoads() {
+    boolean isLoaded = true;
+    assertTrue(isLoaded);
   }
 
 }
