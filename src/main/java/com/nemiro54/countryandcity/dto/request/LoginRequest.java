105ac50ffaa3b0e78e821fcec2b63e2313d7c6ef
Uladzislau Niamira <nemiro54@gmail.com>
2023-08-24 22:42:19 +0300

    dockerize application

--- src/main/java/com/nemiro54/countryandcity/dto/request/LoginRequest.java
+++ src/main/java/com/nemiro54/countryandcity/dto/request/LoginRequest.java
@@ -3,7 +3,7 @@ package com.nemiro54.countryandcity.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record LoginRequest(
-    @Schema(example = "nemiro54")
+    @Schema(example = "test")
     String username,
     @Schema(example = "123")
     String password
--- src/main/java/com/nemiro54/countryandcity/dto/request/RegisterRequest.java
+++ src/main/java/com/nemiro54/countryandcity/dto/request/RegisterRequest.java
@@ -3,9 +3,9 @@ package com.nemiro54.countryandcity.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record RegisterRequest(
-    @Schema(example = "nemiro54")
+    @Schema(example = "test")
     String username,
-    @Schema(example = "nemiro54@gmail.com")
+    @Schema(example = "test@gmail.com")
     String email,
     @Schema(example = "123")
     String password
