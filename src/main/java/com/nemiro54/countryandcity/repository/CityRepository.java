7f6f2c254d13cccd96dfde7f752e44026031d3a5
Uladzislau Niamira <nemiro54@gmail.com>
2023-08-25 12:08:24 +0300

    solve N+1 problem, update docker-compose.yml

--- src/main/java/com/nemiro54/countryandcity/repository/CityRepository.java
+++ src/main/java/com/nemiro54/countryandcity/repository/CityRepository.java
@@ -5,13 +5,20 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, UUID> {
 
+  @EntityGraph(attributePaths = "country")
   Page<City> findCitiesByCountryName(String countryName, Pageable pageable);
 
+  @EntityGraph(attributePaths = "country")
   List<City> findCityByNameIgnoreCase(String name);
+
+  @Override
+  @EntityGraph(attributePaths = "country")
+  Page<City> findAll(Pageable pageable);
 }
